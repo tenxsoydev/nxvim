5b8a3ed531cbb28ccb51d2ea209032b7256f7ffc
Turiiya <34311583+tobealive@users.noreply.github.com>
2023-02-15 23:28:16 +0100

    feat: add lsp on_attach to rust-tools

--- lua/nxvim/lsp/plugins/rust-tools.lua
+++ lua/nxvim/lsp/plugins/rust-tools.lua
@@ -1,5 +1,14 @@
 -- https://github.com/simrat39/rust-tools.nvim
 
-local function init() require("rust-tools").setup() end
+local function init()
+	require("rust-tools").setup({
+		server = {
+			on_attach = function(_, bufnr)
+				require("nxvim.lsp").on_attach()
+				require("nxvim.lsp.plugins.lspsaga").on_attach()
+			end,
+		},
+	})
+end
 
 nx.au({ "Filetype", once = true, pattern = "rust", callback = init })
