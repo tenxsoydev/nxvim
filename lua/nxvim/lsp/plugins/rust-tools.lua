b2f8309618b9c773bed7d9bf7b53d0ab0db71511
Turiiya <34311583+tobealive@users.noreply.github.com>
2023-02-21 12:47:20 +0100

    feat(rust): conditionally disable semantic highlighting
    - improves current defaults theme compatibility

--- lua/nxvim/lsp/plugins/rust-tools.lua
+++ lua/nxvim/lsp/plugins/rust-tools.lua
@@ -2,9 +2,16 @@
 
 require("rust-tools").setup({
 	server = {
-		on_attach = function(_, bufnr)
+		on_attach = function(client, bufnr)
 			require("nxvim.lsp").on_attach(_, bufnr)
 			require("nxvim.lsp.plugins.lspsaga").on_attach(_, bufnr)
+			if vim.g.colors_name == "dracula" then
+				vim.schedule(function() vim.lsp.semantic_tokens.stop(bufnr, client.id) end)
+				nx.cmd({
+					{ "DisableSTH", function() vim.lsp.semantic_tokens.stop(bufnr, client.id) end },
+					{ "EnableSTH", function() vim.lsp.semantic_tokens.start(bufnr, client.id) end },
+				})
+			end
 		end,
 	},
 })
