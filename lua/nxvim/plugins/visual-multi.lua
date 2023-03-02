ecdb6fee829cf9e1c6ac94bcfb3f69bc00773216
Turiiya <34311583+tobealive@users.noreply.github.com>
2023-03-02 22:51:49 +0100

    feat(visual-multi): add VM undo / redo mappings

--- lua/nxvim/plugins/visual-multi.lua
+++ lua/nxvim/plugins/visual-multi.lua
@@ -8,11 +8,13 @@ vim.g.VM_mouse_mappings = 1
 vim.g.VM_maps = {
 	["Find Under"] = "<A-n>",
 	["Find Subword Under"] = "<A-n>",
+	["Undo"] = "u",
+	["Redo"] = "<C-r>",
+	["Add Cursor Up"] = "<A-C-k>",
+	["Add Cursor Down"] = "<A-C-j>",
 	-- Remap to use Shift Left / Right for horizontal scrolling
 	["Select l"] = "<A-C-Right>",
 	["Select h"] = "<A-C-Left>",
-	["Add Cursor Up"] = "<A-C-k>",
-	["Add Cursor Down"] = "<A-C-j>",
 }
 
 -- <== }
