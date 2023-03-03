67efd49e58af9c14f524fa84e8a86d90177b3504
Turiiya <34311583+tobealive@users.noreply.github.com>
2023-03-03 01:56:27 +0100

    feat(tree): use trash for delete_visual

--- lua/nxvim/plugins/neo-tree.lua
+++ lua/nxvim/plugins/neo-tree.lua
@@ -140,11 +140,34 @@ local config = {
 			delete = function(state)
 				local inputs = require("neo-tree.ui.inputs")
 				local path = state.tree:get_node().path
-				local msg = "Are you sure you want to delete " .. path
+				local msg = "Are you sure you want to trash " .. path
 				inputs.confirm(msg, function(confirmed)
 					if not confirmed then return end
 
 					vim.fn.system({ "trash", vim.fn.fnameescape(path) })
+					require("neo-tree.sources.manager").refresh(state.name)
+				end)
+			end,
+			-- over write default 'delete_visual' command to 'trash' x n.
+			delete_visual = function(state, selected_nodes)
+				local inputs = require("neo-tree.ui.inputs")
+
+				-- get table items count
+				function GetTableLen(tbl)
+					local len = 0
+					for _ in pairs(tbl) do
+						len = len + 1
+					end
+					return len
+				end
+
+				local count = GetTableLen(selected_nodes)
+				local msg = "Are you sure you want to trash " .. count .. " files ?"
+				inputs.confirm(msg, function(confirmed)
+					if not confirmed then return end
+					for _, node in ipairs(selected_nodes) do
+						vim.fn.system({ "trash", vim.fn.fnameescape(node.path) })
+					end
 					require("neo-tree.sources.manager").refresh(state.name)
 				end)
 			end,
