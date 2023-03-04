af013fed58bcd1a0e20ea7de61d53faf4a9d6a03
Turiiya <34311583+tobealive@users.noreply.github.com>
2023-03-04 18:22:58 +0100

    fix: disable codeium in telescope prompts

--- lua/nxvim/plugins/codeium.lua
+++ lua/nxvim/plugins/codeium.lua
@@ -1,5 +1,10 @@
 -- https://github.com/Exafunction/codeium.vim
 
+-- { == Events ==> ============================================================
+
+nx.au({ "FileType", pattern = "TelescopePrompt", callback = function() vim.cmd("Codeium DisableBuffer") end })
+-- <== }
+
 --- { == Keymaps ==> ===========================================================
 
 nx.hl({ "CodeiumSuggestion", link = "SpecialComment" })
