8ab7834c382254d5f17dabd03ae5eb8b154a17ca
Turiiya <34311583+tobealive@users.noreply.github.com>
2023-03-27 11:28:56 +0200

    tweak(zen): update config
    * Statusline is not working with the latest vvim version, therefore
      increase height.
    * Fix foldcol chars
    * Disable window switching keymaps to not move out of zen-mode

--- lua/nxvim/plugins/zen-mode.lua
+++ lua/nxvim/plugins/zen-mode.lua
@@ -1,34 +1,21 @@
+-- https://github.com/folke/zen-mode.nvim
+
 local zen_mode = require("zen-mode")
 
 -- { == Configuration ==> =====================================================
 
 zen_mode.setup({
 	window = {
-		-- backdrop = 0.96, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
-		-- height and width can be:
-		-- * an absolute number of cells when > 1
-		-- * a percentage of the width / height of the editor when <= 1
-		-- * a function that returns the width or the height
-		width = 126, -- width of the Zen window
-		height = function() return vim.fn.winheight(0) + 1 end, -- height of the Zen window
-		-- by default, no options are changed for the Zen window
-		-- uncomment any of the options below, or add other vim.wo options you want to apply
-	},
-	plugins = {
-		-- disable some global vim options (vim.o...)
-		-- comment the lines to not apply the options
-		options = {
-			enabled = true,
-			ruler = false, -- disables the ruler text in the cmd line area
-			showcmd = false, -- disables the command in the last line of the screen
-		},
+		--[[ width = tonumber(string.match(vim.o.colorcolumn, "%d+")) + vim.o.foldcolumn + vim.o.numberwidth + tonumber(
+			string.match(vim.o.signcolumn, "%d+")
+		), ]]
+		width = 126,
+		height = function() return vim.fn.winheight(0) + 3 end, -- height of the Zen window
 	},
 	on_open = function()
 		vim.g.zen_mode = true
 		vim.o.laststatus = 3
-		vim.wo.number = false
-		vim.wo.relativenumber = false
-		-- vim.diagnostic.disable()
+		vim.wo.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:›,vert:▏]]
 		--[[ if not vim.g.neovide then return end
 		vim.g.neovide_floating_blur = 1
 		vim.g.neovide_floating_opacity = 0.98 ]]
@@ -36,7 +23,6 @@ zen_mode.setup({
 	on_close = function()
 		vim.g.zen_mode = nil
 		vim.o.laststatus = 2
-		-- vim.diagnostic.enable()
 		--[[ if not vim.g.neovide then return end
 		vim.g.neovide_floating_opacity = 0.8
 		vim.g.neovide_floating_blur = 0.9 ]]
@@ -49,5 +35,9 @@ zen_mode.setup({
 nx.map({
 	{ "<leader>z", "<Cmd>ZenMode<CR>", desc = "Toggle Zen Mode", wk_label = "Zen Mode" },
 	{ "<C-z>", "<Cmd>ZenMode<CR>", desc = "Toggle Zen Mode" },
+	{ "<C-h>", "<C-h>" },
+	{ "<C-j>", "<C-j>" },
+	{ "<C-k>", "<C-k>" },
+	{ "<C-l>", "<C-l>" },
 })
 -- <== }
