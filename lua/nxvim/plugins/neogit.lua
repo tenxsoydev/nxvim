1384bc0012a9dbd75889fdb0e240122a408360e1
Turiiya <34311583+tobealive@users.noreply.github.com>
2023-03-28 18:55:42 +0200

    refactor(neogit): don't be to strict about lazy loading
    * Removes init() function that called setup when entering Neogit
      (No more need for this heavy laziness since moving to lazy.nvim)

--- lua/nxvim/plugins/neogit.lua
+++ lua/nxvim/plugins/neogit.lua
@@ -1,112 +1,103 @@
-local function init()
-	local neogit = require("neogit")
+-- https://github.com/TimUntersberger/neogit
 
-	-- { == Configuration ==> =====================================================
+local neogit = require("neogit")
 
-	local config = {
-		disable_signs = false,
-		disable_hint = false,
-		disable_context_highlighting = false,
-		disable_commit_confirmation = false,
-		disable_insert_on_commit = true,
-		-- Neogit refreshes its internal state after specific events, which can be expensive depending on the repository size.
-		-- Disabling `auto_refresh` will make it so you have to manually refresh the status after you open it.
-		auto_refresh = true,
-		disable_builtin_notifications = true,
-		use_magit_keybindings = false,
-		commit_popup = {
-			kind = "split",
+-- { == Configuration ==> =====================================================
+
+local config = {
+	disable_signs = false,
+	disable_hint = false,
+	disable_context_highlighting = false,
+	disable_commit_confirmation = false,
+	disable_insert_on_commit = true,
+	-- Neogit refreshes its internal state after specific events, which can be expensive depending on the repository size.
+	-- Disabling `auto_refresh` will make it so you have to manually refresh the status after you open it.
+	auto_refresh = true,
+	disable_builtin_notifications = true,
+	use_magit_keybindings = false,
+	commit_popup = {
+		kind = "split",
+	},
+	-- Change the default way of opening neogit
+	kind = "tab",
+	-- customize displayed signs
+	signs = {
+		-- { CLOSED, OPENED }
+		section = { "›", "" },
+		item = { "›", "" },
+		hunk = { "", "" },
+	},
+	integrations = {
+		-- Neogit only provides inline diffs. If you want a more traditional way to look at diffs, you can use `sindrets/diffview.nvim`.
+		-- The diffview integration enables the diff popup, which is a wrapper around `sindrets/diffview.nvim`.
+		--
+		-- Requires you to have `sindrets/diffview.nvim` installed.
+		-- use {
+		--   'TimUntersberger/neogit',
+		--   requires = {
+		--     'nvim-lua/plenary.nvim',
+		--     'sindrets/diffview.nvim'
+		--   }
+		-- }
+		--
+		diffview = true,
+	},
+	-- Setting any section to `false` will make the section not render at all
+	sections = {
+		untracked = {
+			folded = false,
 		},
-		-- Change the default way of opening neogit
-		kind = "tab",
-		-- customize displayed signs
-		signs = {
-			-- { CLOSED, OPENED }
-			section = { "›", "" },
-			item = { "›", "" },
-			hunk = { "", "" },
+		unstaged = {
+			folded = false,
 		},
-		integrations = {
-			-- Neogit only provides inline diffs. If you want a more traditional way to look at diffs, you can use `sindrets/diffview.nvim`.
-			-- The diffview integration enables the diff popup, which is a wrapper around `sindrets/diffview.nvim`.
-			--
-			-- Requires you to have `sindrets/diffview.nvim` installed.
-			-- use {
-			--   'TimUntersberger/neogit',
-			--   requires = {
-			--     'nvim-lua/plenary.nvim',
-			--     'sindrets/diffview.nvim'
-			--   }
-			-- }
-			--
-			diffview = true,
+		staged = {
+			folded = false,
 		},
-		-- Setting any section to `false` will make the section not render at all
-		sections = {
-			untracked = {
-				folded = false,
-			},
-			unstaged = {
-				folded = false,
-			},
-			staged = {
-				folded = false,
-			},
-			stashes = {
-				folded = true,
-			},
-			unpulled = {
-				folded = true,
-			},
-			unmerged = {
-				folded = false,
-			},
-			recent = {
-				folded = true,
-			},
+		stashes = {
+			folded = true,
 		},
-		mappings = {},
-	}
-	-- <== }
-
-	-- { == Events ==> ===========================================================
-
-	nx.au({ "FileType", pattern = "NeogitCommitMessage", command = "setlocal spell" })
-	-- <== }
-
-	-- { == Keymaps ==> ==========================================================
-
-	config.mappings = {
-		status = {
-			-- Adds a mapping with "B" as key that does the "BranchPopup" command
-			["B"] = "BranchPopup",
-			["<kEnter>"] = "GoToFile",
-			-- E.g., remove the default mapping of "s"
-			-- ["s"] = "",
+		unpulled = {
+			folded = true,
 		},
-
-		commit = {
-			send = { ";q", "<leader>e" },
+		unmerged = {
+			folded = false,
 		},
-	}
+		recent = {
+			folded = true,
+		},
+	},
+	mappings = {},
+}
+-- <== }
 
-	nx.au({ "Filetype", pattern = "NeogitPushPopup", command = "nnoremap <buffer> F <nop>" })
-	-- <== }
+-- { == Events ==> ===========================================================
 
-	-- { == Highlights ==> =======================================================
+nx.au({ "FileType", pattern = "NeogitCommitMessage", command = "setlocal spell" })
+-- <== }
 
-	if vim.g.colors_name == "dracula" then
-		nx.hl({ "NeogitDiffAddHighlight", fg = "DiffAdd:fg", bg = "StatusLineTermNC:bg" })
-		-- fix blue difftext in Neogit
-		nx.hl({ "NeogitDiffDeleteHighlight", fg = "DraculaDiffDelete:fg", bg = "StatusLineTermNC:bg" })
-	end
-	-- <== }
+-- { == Keymaps ==> ==========================================================
 
-	neogit.setup(config)
+config.mappings = {
+	status = {
+		["B"] = "BranchPopup",
+		["<kEnter>"] = "GoToFile",
+	},
+	commit = {
+		send = { ";q", "<leader>e" },
+	},
+}
+
+nx.map({ "<leader>gn", "<Cmd>Neogit<CR>", desc = "Neogit" })
+nx.au({ "Filetype", pattern = "NeogitPushPopup", command = "nnoremap <buffer> F <nop>" })
+-- <== }
+
+-- { == Highlights ==> =======================================================
+
+if vim.g.colors_name == "dracula" then
+	nx.hl({ "NeogitDiffAddHighlight", fg = "DiffAdd:fg", bg = "StatusLineTermNC:bg" })
+	-- fix blue difftext in Neogit
+	nx.hl({ "NeogitDiffDeleteHighlight", fg = "DraculaDiffDelete:fg", bg = "StatusLineTermNC:bg" })
 end
+-- <== }
 
--- Pre-loaded keymaps - allow init via shortcut
-nx.map({ { "<leader>gn", "<Cmd>Neogit<CR>", desc = "Neogit" } })
-
--- Lazy init on filetype enter
-nx.au({ "FileType", once = true, pattern = "NeogitStatus", callback = init })
+neogit.setup(config)
