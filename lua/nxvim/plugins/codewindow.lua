fe63a51c89b43be181ece9d1897db678f5fcc014
Turiiya <34311583+tobealive@users.noreply.github.com>
2023-04-18 19:12:43 +0200

    minimap: update style

--- lua/nxvim/plugins/codewindow.lua
+++ lua/nxvim/plugins/codewindow.lua
@@ -1,3 +1,5 @@
+-- https://github.com/gorbit99/codewindow.nvim
+
 local codewindow = require("codewindow")
 
 -- { == Configuration ==> =====================================================
@@ -8,15 +10,16 @@ codewindow.setup({
 	exclude_filetypes = {}, -- Choose certain filetypes to not show minimap on
 	max_minimap_height = nil, -- The maximum height the minimap can take (including borders)
 	max_lines = nil, -- If auto_enable is true, don't open the minimap for buffers which have more than this many lines.
-	minimap_width = 16, -- The width of the text part of the minimap
+	minimap_width = 17, -- The width of the text part of the minimap
 	use_lsp = true, -- Use the builtin LSP to show errors and warnings
 	use_treesitter = true, -- Use nvim-treesitter to highlight the code
 	use_git = true, -- Show small dots to indicate git additions and deletions
 	width_multiplier = 4, -- How many characters one dot represents
 	z_index = 1, -- The z-index the floating window will be on
 	show_cursor = true, -- Show the cursor position in the minimap
-	window_border = nx.opts.float_win_border, -- The border style of the floating window (accepts all usual options)
+	window_border = { "▕", " ", " ", " ", " ", " ", "▕", "▕" }, -- The border style of the floating window (accepts all usual options)
 })
+
 -- <== }}
 
 -- { == Keymaps ==> ===========================================================
