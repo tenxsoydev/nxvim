4166ab8210677ec02e97d21bfa3f10760a651e5b
Turiiya <34311583+tobealive@users.noreply.github.com>
2023-05-19 01:06:26 +0200

    multi-cursor: update leader-key

--- lua/nxvim/plugins/visual-multi.lua
+++ lua/nxvim/plugins/visual-multi.lua
@@ -4,7 +4,9 @@ vim.opt.rtp:prepend(vim.fn.stdpath("data") .. "/lazy/visual-multi")
 
 -- { == Keymaps ==> ===========================================================
 
+-- https://github.com/mg979/vim-visual-multi/wiki/Mappings
 vim.g.VM_mouse_mappings = 1
+vim.g.VM_leader = [[\]]
 vim.g.VM_maps = {
 	["Find Under"] = vim.g.eu_kbd and "ñ" or "<A-n>",
 	["Find Subword Under"] = vim.g.eu_kbd and "ñ" and "<A-n>",
