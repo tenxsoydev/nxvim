4ad7831fdb11bf6cbf1744e47d2b0011da0c7b05
Turiiya <34311583+tobealive@users.noreply.github.com>
2023-07-01 02:18:33 +0200

    window-picker: use latest upstream version

--- lua/nxvim/plugins/window-picker.lua
+++ lua/nxvim/plugins/window-picker.lua
@@ -3,23 +3,14 @@
 -- { == Configuration ==> =====================================================
 
 require("window-picker").setup({
-	autoselect_one = true,
-	include_current = false,
-	filter_rules = {
-		-- filter using buffer options
-		bo = {
-			-- if the file type is one of following, the window will be ignored
-			filetype = { "neo-tree", "neo-tree-popup", "notify", "quickfix" },
-
-			-- if the buffer type is one of following, the window will be ignored
-			buftype = { "terminal" },
+	highlights = {
+		statusline = {
+			unfocused = {
+				bg = require("nxvim.utils").get_hl("StatusLine", "bg"),
+				bold = true,
+			},
 		},
 	},
-})
-
-nx.hl({
-	{ "NvimWindoSwitchNC", bg = "StatusLineNC:bg" },
-	{ "NvimWindoSwitch", bg = "StatusLine:bg" },
 })
 
 -- <== }
