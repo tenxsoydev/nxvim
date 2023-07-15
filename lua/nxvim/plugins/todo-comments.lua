8b4dffedd7c4b9ef1b02966cb7635d14df8334a7
Turiiya <34311583+tobealive@users.noreply.github.com>
2023-07-15 05:30:23 +0200

    todo-comments: update hack icon, require colon for highlight

--- lua/nxvim/plugins/todo-comments.lua
+++ lua/nxvim/plugins/todo-comments.lua
@@ -16,7 +16,7 @@ require("todo-comments").setup({
 		TODO = { icon = " ", alt = { "#to/do" } },
 		NOTE = { icon = "󰍨 ", alt = { "INFO" } },
 		WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
-		HACK = { icon = " ", color = "warning" },
+		HACK = { icon = "󰈸 ", color = "warning" },
 		PERF = { icon = "󰅒 ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
 		TEST = { icon = "󰕥 ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
 		DBG = { icon = " " },
@@ -37,7 +37,7 @@ require("todo-comments").setup({
 		before = "", -- "fg" or "bg" or empty
 		keyword = "wide_fg", -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
 		after = "", -- "fg" or "bg" or empty
-		pattern = [[.*<(KEYWORDS)\s*]], -- pattern or table of patterns, used for highlightng (vim regex)
+		pattern = [[.*<(KEYWORDS):\s*]], -- pattern or table of patterns, used for highlightng (vim regex)
 		comments_only = true, -- uses treesitter to match keywords in comments only
 		max_line_len = 400, -- ignore lines longer than this
 		exclude = {}, -- list of file types to exclude highlighting
