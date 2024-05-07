4344eed6fc12a728b9a2fae15e52dfad4d405d8e
Turiiya <34311583+ttytm@users.noreply.github.com>
2024-05-07 23:52:19 +0200

    bookmarks: use new settings in config

--- lua/nxvim/plugins/bookmarks.lua
+++ lua/nxvim/plugins/bookmarks.lua
@@ -1,11 +1,16 @@
--- https://github.com/tomasky/bookmarks.nvim
+-- https://github.com/LintaoAmons/bookmarks.nvim
 
-require("bookmarks")
+require("bookmarks").setup({
+	signs = {
+		mark = { icon = "󰃃", color = "grey" },
+		annotation = { icon = "󰆉", color = "grey" },
+	},
+})
 
 -- { == Keymaps ==> ===========================================================
 
 nx.map({
-	{ "<leader>mm", "<Cmd>BookmarksMark<CR>", desc = "Bookmark toggle", wk_label = "Toggle", },
+	{ "<leader>mm", "<Cmd>BookmarksMark<CR>", desc = "Bookmark toggle", wk_label = "Toggle" },
 	{ "<leader>/m", "<Cmd>BookmarksGoto<CR>", desc = "Search Bookmarks", wk_label = "Bookmarks" },
 	{ "<leader>m/", "<Cmd>BookmarksGoto<CR>", desc = "Search Bookmarks", wk_label = "Search" },
 	{ "<leader>m.", "<Cmd>BookmarksGotoRecent<CR>", desc = "Search Bookmarks", wk_label = "Search" },
