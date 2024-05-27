62f71765ba3167ac2c31112daa4475d4aef5a521
Turiiya <34311583+ttytm@users.noreply.github.com>
2024-05-27 23:39:29 +0200

    chore: add load events to mini and neo-scroll plugin

--- lua/nxvim/init.lua
+++ lua/nxvim/init.lua
@@ -206,8 +206,9 @@ local modules = {
 	{ "kevinhwang91/nvim-hlslens", event = "VeryLazy", config = "plugins.hlslens" },
 	{ "edluffy/hologram.nvim", event = "VeryLazy" },
 	{ "lukas-reineke/indent-blankline.nvim", event = "VeryLazy", config = "plugins.indentline" },
-	{ "echasnovski/mini.nvim", config = "plugins.mini" },
-	{ "karb94/neoscroll.nvim", config = "plugins.neoscroll", commit = "e78657719485c5663b88e5d96ffcfb6a2fe3eec0" },
+	{ "echasnovski/mini.nvim", event = "VeryLazy", config = "plugins.mini" },
+	-- stylua: ignore
+	{ "karb94/neoscroll.nvim", event = "VeryLazy", config = "plugins.neoscroll", commit = "e78657719485c5663b88e5d96ffcfb6a2fe3eec0" },
 	{ "nacro90/numb.nvim", event = "VeryLazy", config = true },
 	{ "windwp/nvim-autopairs", event = "VeryLazy", config = "plugins.autopairs" },
 	{ "NvChad/nvim-colorizer.lua", event = "VeryLazy", config = "plugins.colorizer" },
