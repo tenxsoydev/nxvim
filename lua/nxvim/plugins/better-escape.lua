5b0dd172d9faa23d5897a6d40a08a729ed8adc65
Turiiya <34311583+ttytm@users.noreply.github.com>
2024-07-12 00:23:46 +0200

    better-escape: add mappings config after recent upstream changes

---
+++ lua/nxvim/plugins/better-escape.lua
@@ -0,0 +1,35 @@
+-- https://github.com/max397574/better-escape.nvim
+
+-- { == Configuration ==> ====================================================
+
+require("better_escape").setup({
+	default_mappings = false,
+	mappings = {
+		i = {
+			j = {
+				k = "<Esc>",
+			},
+		},
+		c = {
+			j = {
+				k = "<Esc>",
+			},
+		},
+		t = {
+			j = {
+				k = "<Esc>",
+			},
+		},
+		v = {
+			j = {
+				k = "<Esc>",
+			},
+		},
+		s = {
+			j = {
+				k = "<Esc>",
+			},
+		},
+	},
+})
+-- <== }
