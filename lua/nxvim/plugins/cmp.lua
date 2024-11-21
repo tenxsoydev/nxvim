28a9109281b8207611fc24ee90ca81a1ae764986
Turiiya <34311583+ttytm@users.noreply.github.com>
2024-11-21 02:52:30 +0100

    cmp: commented out tab cycle mappings

--- lua/nxvim/plugins/cmp.lua
+++ lua/nxvim/plugins/cmp.lua
@@ -107,32 +107,6 @@ config.mapping = {
 			fallback()
 		end
 	end, { "i", "s" }),
-
-	-- Use tab to cycles through completion suggestions
-	--[[ ["<Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif require("luasnip").expand_or_jumpable() then
-				vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
-			else
-				fallback()
-			end
-		end, {
-			"i",
-			"s",
-		}),
-		["<S-Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif require("luasnip").jumpable(-1) then
-				vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
-			else
-				fallback()
-			end
-		end, {
-			"i",
-			"s",
-		}), ]]
 }
 -- ]
 
