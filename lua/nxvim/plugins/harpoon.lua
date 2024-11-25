15f5f780944d0350c50a1173ac4acfb66fd3f183
Turiiya <34311583+ttytm@users.noreply.github.com>
2024-11-25 11:53:32 +0100

    harpoon: add capital `M` mappings
    middle of line keymap that I personally do not use. But it will still
    work with this change after `timeoutlen`.

--- lua/nxvim/plugins/harpoon.lua
+++ lua/nxvim/plugins/harpoon.lua
@@ -7,13 +7,13 @@ require("harpoon").setup()
 local harpoon_ui = require("harpoon.ui")
 
 nx.map({
-	{ "mh/", function() harpoon_ui.toggle_quick_menu() end, desc = "Search Harpoons" },
-	{ "mh1", function() harpoon_ui.nav_file(1) end, desc = "Go to Harpoon #1" },
-	{ "mh2", function() harpoon_ui.nav_file(2) end, desc = "Go to Harpoon #2" },
-	{ "mh3", function() harpoon_ui.nav_file(3) end, desc = "Go to Harpoon #3" },
-	{ "mh4", function() harpoon_ui.nav_file(4) end, desc = "Go to Harpoon #4" },
+	{ { "mh/", "<S-m>/" }, function() harpoon_ui.toggle_quick_menu() end, desc = "Search Harpoons" },
+	{ { "mh1", "<S-m>1" }, function() harpoon_ui.nav_file(1) end, desc = "Go to Harpoon #1" },
+	{ { "mh2", "<S-m>2" }, function() harpoon_ui.nav_file(2) end, desc = "Go to Harpoon #2" },
+	{ { "mh3", "<S-m>3" }, function() harpoon_ui.nav_file(3) end, desc = "Go to Harpoon #3" },
+	{ { "mh4", "<S-m>4" }, function() harpoon_ui.nav_file(4) end, desc = "Go to Harpoon #4" },
 	-- stylua: ignore
-	{ "mha", function() require("harpoon.mark").add_file() end, desc = "Harpoon Current File", wk_label =  "Add Current File" },
+	{ "<S-m>a", function() require("harpoon.mark").add_file() end, desc = "Harpoon Current File", wk_label =  "Add Current File" },
 }, { wk_label = { sub_desc = "Harpoon[s]?" } })
 
 nx.map({
