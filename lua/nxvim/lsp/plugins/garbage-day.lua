b26ffad3003ba4db42cafa21abed22c19c3f1281
Turiiya <34311583+ttytm@users.noreply.github.com>
2024-12-10 11:49:38 +0100

    lsp: add missing garbage-day config file

---
+++ lua/nxvim/lsp/plugins/garbage-day.lua
@@ -0,0 +1,9 @@
+-- https://github.com/Zeioth/garbage-day.nvim
+
+-- == [ Configuration =========================================================
+
+require("garbage-day").setup({
+	excluded_lsp_clients = { "null-ls" },
+	-- "null-ls", "jdtls", "marksman", "lua_ls"
+})
+-- ]
