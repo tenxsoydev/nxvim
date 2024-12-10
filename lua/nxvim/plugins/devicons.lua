539d1321617769bd41d643a33971098f62d19976
Turiiya <34311583+ttytm@users.noreply.github.com>
2024-12-10 11:37:29 +0100

    devicons: add icons for more filetypes: c3, njk, odin, onyx

--- lua/nxvim/plugins/devicons.lua
+++ lua/nxvim/plugins/devicons.lua
@@ -1,20 +1,53 @@
 -- https://github.com/nvim-tree/nvim-web-devicons
 
 -- == [ Configuration =========================================================
+local c3 = {
+	icon = "󱤹",
+	color = "#7986cb",
+	cterm_color = "104",
+	name = "C3",
+}
 
 require("nvim-web-devicons").setup({
 	override_by_extension = {
+		["c3"] = c3,
+		["c3i"] = c3,
+		["c3t"] = c3,
 		["njk"] = {
 			icon = "",
 			color = "#e44d26",
 			cterm_color = "196",
 			name = "Nunjucks",
 		},
+		["odin"] = {
+			icon = "󰗐",
+			color = "#4285F4",
+			cterm_color = "33",
+			name = "Odin",
+		},
+		["onyx"] = {
+			icon = "󰫆",
+			color = "#6d8086",
+			cterm_color = "66",
+			name = "Onyx",
+		},
+		["onyx-pkg.kdl"] = {
+			icon = "",
+			color = "#41535b",
+			cterm_color = "239",
+			name = "Onyx",
+		},
 		["py"] = {
 			icon = "",
 			color = "#4B8BBE",
 			cterm_color = "214",
 			name = "Py",
+		},
+		["sp"] = {
+			icon = "󱗆",
+			color = "#4B8BBE",
+			cterm_color = "214",
+			name = "Spawn",
 		},
 		["v"] = {
 			icon = "",
