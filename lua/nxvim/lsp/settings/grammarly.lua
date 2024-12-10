6e5cc0efb1a8634b6aa01f62524e9ca142360a49
Turiiya <34311583+ttytm@users.noreply.github.com>
2024-12-10 11:29:23 +0100

    lsp(grammarly): remove settings since having replaced grammarly with ltex

--- lua/nxvim/lsp/settings/grammarly.lua
+++
@@ -1,8 +0,0 @@
-return {
-	--[[ E.g.: grammarly.sh
-#!/usr/bin/env bash
-source "${NVM_DIR}/nvm.sh"
-nvm run 16 ~/.local/share/nvim/mason/bin/grammarly-languageserver --stdio
-]]
-	cmd = { "/home/t/grammarly.sh" },
-}
