89590bbaed3ee9660ee3d2999bccfbe7bc6f76c1
Turiiya <34311583+ttytm@users.noreply.github.com>
2025-08-16 12:23:13 +0200

    hl: fix setting kitty theme when changing colorscheme

--- lua/nxvim/colorschemes/init.lua
+++ lua/nxvim/colorschemes/init.lua
@@ -1,22 +1,22 @@
 local default_colorscheme = "dracula"
--- changing a colorscheme during a session can partially break highlights
+-- Changing a colorscheme during a session can partially break highlights
 -- to have an unimaired experience, define a theme of preference as default and restart vim
 
--- make sure to load colorscheme setup before applying the colorscheme
+-- Load colorscheme setup BEFORE applying the colorscheme
 pcall(require("nxvim.colorschemes." .. default_colorscheme))
-
 -- Apply default colorscheme
 vim.cmd("colorscheme " .. default_colorscheme)
 
 -- == [ Highlights ============================================================
 
----Set kitty colorscheme based on neovim colorscheme.
----@param colors string @kitty colorscheme file
-local function kitty_colors(colors)
-	if not vim.fn.executable("kitty") then return end
-	local cmd = "kitty @ --to %s set-colors -a %s"
+---Set kitty colorscheme based on neovim colorscheme
+-- TODO: add further commenting about kitty theme files
+---@param color_scheme string @kitty colorscheme file
+local function set_kitty_colors(color_scheme)
 	local socket = vim.fn.expand("$KITTY_LISTEN_ON")
-	vim.fn.system(cmd:format(socket, "~/.config/kitty/" .. colors .. ".conf"))
+	local color_scheme_path = "~/.config/kitty/themes/" .. color_scheme .. ".conf"
+	if vim.fn.filereadable(vim.fn.expand(color_scheme_path)) == 0 then return end
+	vim.fn.system("kitten @ --to " .. socket .. " set-colors " .. color_scheme_path)
 	vim.cmd("redraw")
 end
 
@@ -27,11 +27,13 @@ local function set_hl()
 		{ { "Winbar", "FoldColumn" }, link = "LineNr" },
 	})
 
+	if vim.env.TERM == "xterm-kitty" then
+		set_kitty_colors(vim.g.colors_name == "ayu" and "ayu_mirage" or vim.g.colors_name)
+	end
+
 	if vim.g.colors_name == "dracula" then
-		kitty_colors("dracula")
 		require("nxvim.colorschemes.dracula").set_hl()
 	elseif vim.g.colors_name == "tokyonight" then
-		kitty_colors("tokyonight_storm")
 		require("nxvim.colorschemes.tokyonight").set_hl()
 	end
 end
